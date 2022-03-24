f44fc75671ae56e3f5425f5494b295ceb9608a5e
Jacob <jacobmjones17@gmail.com>
2022-03-24 14:09:05 -0400

    commit tree 2dc45f3de1d0809eb2b9c95db69ad62961ca0c4b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 let addToy = false;
 
+const imageObj = {
+  method: "GET",
+  headers: {
+      "Content-Type" : "application/json",
+      Accept: "application/json",
+  },
+  body: JSON.stringify(),
+};
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -13,3 +25,49 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function showToy(toy) {
+  const grabElement = document.getElementById("toy-collection")
+  const newClass = document.createElement("div");
+  newClass.setAttribute("class", "card");
+  grabElement.append(newClass);
+};
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    data.forEach(toy => showToy(toy));
+
+    console.log(data)
+    document.querySelector("#toy-collection").textContent = object
+  })
+};
+
+function fetchImage() {
+  fetch("http://localhost:3000/toys")
+  .then(function (response) {
+    return response.json
+  })
+  .then(function (image) {
+    image.forEach(toyImage => showImage(toyImage))
+    document.querySelector("body").textContent = object.id
+  })
+}
+
+function showImage(toy) {
+  const img = document.createElement("img")
+  Image.src = toy;
+  const toyImageContainer = document.getElementById("toy-collection")
+  toyImageContainer.append(img)
+}
+
+
+
+
+
+showToy();
+fetchToys();
+showImage()
